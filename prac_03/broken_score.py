e20c9aa42b9feb49b7744c10df3643e4b1306549
PaiHwai22/Pai22 <pai.hwai@my.jcu.edu.au>
2022-04-04 23:55:04 +0630

    create function that returns random score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,24 +2,32 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
 def main():
     score = float(input("Enter score: "))
-
     while score < 0 or score > 100:
         print("invalid score")
         score = float(input("Enter score: "))
     print(judge_score(score))
+    print(generate_random_score())
 
 
 def judge_score(score):
+    """ Return a grade to score"""
     if score >= 90:
         return "Excellent"
     elif 50 <= score < 90:
         return "Passable"
     else:
         return "Bad"
+
+
+def generate_random_score():
+    """ return a random inclusive score"""
+    score = random.randint(1, 100)
+    return score
 
 
 main()
