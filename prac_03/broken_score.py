2bafc3206029423d04ddf7537dd1d310bee0543a
PaiHwai22/Pai22 <pai.hwai@my.jcu.edu.au>
2022-04-04 23:37:51 +0630

    create function that returns statement

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,13 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("invalid score")
+
+def main():
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif 50 <= score < 90:
-    print("Passable")
-else:
-    print("Bad")
+
+    while score < 0 or score > 100:
+        print("invalid score")
+        score = float(input("Enter score: "))
+    print(judge_score(score))
+
+
+def judge_score(score):
+    if score >= 90:
+        return "Excellent"
+    elif 50 <= score < 90:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
